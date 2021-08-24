51195910d48c432cc787c0892fb87c16964eb1c0
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2021-08-24 16:40:36 +0300

    Reformat shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
   sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
+  pkgs = import sources.nixpkgs { };
 in
 pkgs.mkShell {
   nativeBuildInputs = [ pkgs.niv pkgs.nixpkgs-fmt ];
